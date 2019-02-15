72d2f82eb2ef1264cae4a39c72e9863ff795bb39
Rooholamin R <rooholaminrasooli@gmail.com>
2019-02-15 13:17:21 +0330

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,19 +1,21 @@
 #read main data :
 NEI <- readRDS(file.path('pm25em', 'summarySCC_PM25.rds'))
 SCC <- readRDS(file.path('pm25em', 'Source_Classification_Code.rds'))
-##################
+### libs
 library(RColorBrewer)
-#################
-png('plot4.png',
-    width = 500,
-    height = 500,
-    units = 'px')
+### Data Prep
 combustVec <- grep('comb', SCC$SCC.Level.One, ignore.case = T)
 coalVec <- grep('coal', SCC$SCC.Level.Four, ignore.case = T)
 coalcomb <- c(combustVec, coalVec)
 subdata <- SCC[coalcomb,]$SCC
 CRdata <- NEI[NEI$SCC %in% subdata, c(4, 6)]
 CRdata <- tapply(CRdata$Emissions, CRdata$year, sum, na.rm = T)
+
+### Plot
+png('plot4.png',
+    width = 500,
+    height = 500,
+    units = 'px')
 colvec <- brewer.pal(4, 'Purples')
 mybar <-
       barplot(
