ff7963be5af3a1079fdf4101a7b974da91f74b8e
Rooholamin R <rooholaminrasooli@gmail.com>
2019-02-15 12:46:50 +0330

    update plot1.R
    cleaning comments

--- plot1.R
+++ plot1.R
@@ -1,17 +1,20 @@
-# National Emissions Inventory database
-#fine particulate matter pollution in the United states over the 10-year period 1999-2008
 #read main data :
 NEI <- readRDS(file.path('pm25em', 'summarySCC_PM25.rds'))
 
-##################
+### libs
 library(RColorBrewer)
+
+###Data Prep
+
+SummedByYear <-
+      tapply(NEI$Emissions, factor(NEI$year), sum, na.rm = T) / 10 ^ 6 #let's show with millions
+colorVector1 <- brewer.pal(length(SummedByYear), 'GnBu')
+
+### Plot
 png('plot1.png',
     width = 500,
     height = 500,
     units = 'px')
-SummedByYear <-
-      tapply(NEI$Emissions, factor(NEI$year), sum, na.rm = T) / 10 ^ 6 #let's show with millions
-colorVector1 <- brewer.pal(length(SummedByYear), 'GnBu')
 bp <-
       barplot(
             SummedByYear,
