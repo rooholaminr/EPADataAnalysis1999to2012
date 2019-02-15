161abf90e3cd529732ff43d29cf4c0fd2e31c890
Rooholamin R <rooholaminrasooli@gmail.com>
2019-02-15 12:58:46 +0330

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,14 +1,13 @@
-# National Emissions Inventory database
-#fine particulate matter pollution in the United states over the 10-year period 1999-2008
 #read main data :
 NEI <- readRDS(file.path('pm25em', 'summarySCC_PM25.rds'))
-##############
+###libs
 library(RColorBrewer)
-###############
+### Data Prep
 NEI.BAl <- NEI[NEI$fips == "24510",]
 SummedByYear.Bal <-
       tapply(NEI.BAl$Emissions, factor(NEI.BAl$year), sum, na.rm = T)  #let's show with millions
 colorVector2 <- brewer.pal(length(SummedByYear.Bal), 'OrRd')
+### Plot
 png('plot2.png',
     width = 500,
     height = 500,
